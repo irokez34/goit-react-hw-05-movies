9cf0cabdc75768cb819db54dfdb2ec4ae478eedd
irokez34 <jhmdaurel@gmail.com>
2023-12-29 16:52:24 +0200

    fix

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,11 +1,17 @@
 export const MovieInfo = ({ movie }) => {
+  const defaultImg =
+    'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
   return (
     <>
       {movie && (
         <div className="container-movie-info">
           <div className="movie-info">
             <img
-              src={`https://image.tmdb.org/t/p/w400/${movie.backdrop_path}`}
+              src={
+                movie.backdrop_path
+                  ? `https://image.tmdb.org/t/p/w400/${movie.backdrop_path}`
+                  : defaultImg
+              }
               alt={movie.title || movie.name}
               className="movie-img"
             />
